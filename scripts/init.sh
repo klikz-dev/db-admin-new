c977a1a7cdebfd6c650e7f677f1794b2b062807f
Murrell <dev@klikz.us>
2024-03-23 13:29:20 +0000

    chore: init

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,38 +1,38 @@
-# python3 manage.py brewster feed
-# python3 manage.py couture feed
-# python3 manage.py covington feed
-# python3 manage.py danagibson feed
-# python3 manage.py elainesmith feed
-# python3 manage.py exquisiterugs feed
-# python3 manage.py galerie feed
-# python3 manage.py hubbardtonforge feed
-# python3 manage.py jaipurliving feed
-# python3 manage.py jamieyoung feed
-# python3 manage.py jffabrics feed
-# python3 manage.py kasmir feed
-# python3 manage.py kravet feed
-# python3 manage.py kravetdecor feed
-# python3 manage.py materialworks feed
-# python3 manage.py maxwell feed
-# python3 manage.py mindthegap feed
-# python3 manage.py noir feed
-# python3 manage.py oliviaquinn feed
-# python3 manage.py peninsulahome feed
-# python3 manage.py phillipjeffries feed
-# python3 manage.py phillipscollection feed
-# python3 manage.py pindler feed
-# python3 manage.py pkaufman feed
-# python3 manage.py poppy feed
-# python3 manage.py port68 feed
-# python3 manage.py premierprints feed
-# python3 manage.py scalamandre feed
-# python3 manage.py schumacher feed
-# python3 manage.py seabrook feed
-# python3 manage.py stout feed
-# python3 manage.py surya feed
-# python3 manage.py tempaper feed
-# python3 manage.py york feed
-# python3 manage.py zoffany feed
+python3 manage.py brewster feed
+python3 manage.py couture feed
+python3 manage.py covington feed
+python3 manage.py danagibson feed
+python3 manage.py elainesmith feed
+python3 manage.py exquisiterugs feed
+python3 manage.py galerie feed
+python3 manage.py hubbardtonforge feed
+python3 manage.py jaipurliving feed
+python3 manage.py jamieyoung feed
+python3 manage.py jffabrics feed
+python3 manage.py kasmir feed
+python3 manage.py kravet feed
+python3 manage.py kravetdecor feed
+python3 manage.py materialworks feed
+python3 manage.py maxwell feed
+python3 manage.py mindthegap feed
+python3 manage.py noir feed
+python3 manage.py oliviaquinn feed
+python3 manage.py peninsulahome feed
+python3 manage.py phillipjeffries feed
+python3 manage.py phillipscollection feed
+python3 manage.py pindler feed
+python3 manage.py pkaufman feed
+python3 manage.py poppy feed
+python3 manage.py port68 feed
+python3 manage.py premierprints feed
+python3 manage.py scalamandre feed
+python3 manage.py schumacher feed
+python3 manage.py seabrook feed
+python3 manage.py stout feed
+python3 manage.py surya feed
+python3 manage.py tempaper feed
+python3 manage.py york feed
+python3 manage.py zoffany feed
 
 python3 manage.py migrator shopify
 python3 manage.py migrator image
