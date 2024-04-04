35c98f9ca31c92db009bc1db823e639f714fe0f4
Murrell <dev@klikz.us>
2024-04-04 16:20:36 +0000

    fix: order sync

--- vendor/management/commands/sync-order.py
+++ vendor/management/commands/sync-order.py
@@ -1,7 +1,7 @@
 from django.core.management.base import BaseCommand
 from django.db.models import Max
 from tqdm import tqdm
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 from utils import debug, shopify, common
 
@@ -32,7 +32,33 @@ class Processor:
     def __exit__(self, exc_type, exc_val, exc_tb):
         pass
 
+    def address(self, address_data):
+        unique_fields = {
+            'address1': address_data['address1'],
+            'city': address_data['city'],
+            'state': address_data['province'],
+            'zip': address_data['zip'],
+            'country': address_data['country'],
+        }
+
+        defaults = {
+            'firstName': address_data['first_name'],
+            'lastName': address_data['last_name'],
+            'company': address_data.get('company', ''),
+            'address2': address_data.get('address2', ''),
+            'phone': address_data.get('phone', ''),
+        }
+
+        address, _ = Address.objects.get_or_create(
+            **unique_fields, defaults=defaults)
+        return address
+
     def order(self):
+
+        Address.objects.all().delete()
+        Customer.objects.all().delete()
+        LineItem.objects.all().delete()
+        Order.objects.all().delete()
 
         shopifyManager = shopify.ShopifyManager()
 
@@ -48,43 +74,25 @@ class Processor:
 
             # for order in tqdm(orders):
             def syncOrder(order):
+
                 # Addresses
-                def get_or_create_address(address_data):
-                    unique_fields = {
-                        'firstName': address_data['first_name'],
-                        'lastName': address_data['last_name'],
-                        'address1': address_data['address1'],
-                        'city': address_data['city'],
-                        'state': address_data['province'],
-                        'zip': address_data['zip'],
-                        'country': address_data['country'],
-                    }
-
-                    defaults = {
-                        'company': address_data.get('company', ''),
-                        'address2': address_data.get('address2', ''),
-                        'phone': address_data.get('phone', ''),
-                    }
-
-                    return Address.objects.get_or_create(**unique_fields, defaults=defaults)
-
-                customerAddress, created = get_or_create_address(
+                customerAddress = self.address(
                     order['customer']['default_address'])
-                shippingAddress, created = get_or_create_address(
-                    order['shipping_address'])
-                billingAddress, created = get_or_create_address(
-                    order['billing_address'])
+                shippingAddress = self.address(order['shipping_address'])
+                billingAddress = self.address(order['billing_address'])
 
                 # Customer
-                customer, created = Customer.objects.update_or_create(
+                customer, _ = Customer.objects.update_or_create(
                     shopifyId=order['customer']['id'],
-                    email=order['customer']['email'],
-                    firstName=order['customer']['first_name'],
-                    lastName=order['customer']['last_name'],
-                    phone=order['customer']['phone'],
-                    address=customerAddress,
-                    note=order['customer']['note'],
-                    tags=order['customer']['tags'],
+                    defaults={
+                        'email': order['customer']['email'],
+                        'firstName': order['customer']['first_name'],
+                        'lastName': order['customer']['last_name'],
+                        'phone': order['customer']['phone'],
+                        'address': customerAddress,
+                        'note': order['customer']['note'],
+                        'tags': order['customer']['tags'],
+                    }
                 )
 
                 status = ''
@@ -114,32 +122,33 @@ class Processor:
                     shippingCost = 0
                     shippingMethod = "Free Shipping"
 
-                orderRef, created = Order.objects.update_or_create(
+                orderRef, _ = Order.objects.update_or_create(
                     shopifyId=order['id'],
-                    po=order['order_number'],
+                    defaults={
+                        "po": order['order_number'],
 
-                    orderType=orderType,
-                    email=order['email'],
-                    phone=order['phone'],
-                    customer=customer,
+                        "orderType": orderType,
+                        "email": order['email'],
+                        "phone": order['phone'],
+                        "customer": customer,
 
-                    shippingAddress=shippingAddress,
-                    billingAddress=billingAddress,
+                        "shippingAddress": shippingAddress,
+                        "billingAddress": billingAddress,
 
-                    subTotal=order['total_line_items_price'],
-                    discount=order['total_discounts'],
-                    shippingCost=shippingCost,
-                    tax=order['current_total_tax'],
-                    total=order['total_price'],
+                        "subTotal": order['total_line_items_price'],
+                        "discount": order['total_discounts'],
+                        "shippingCost": shippingCost,
+                        "tax": order['current_total_tax'],
+                        "total": order['total_price'],
 
-                    shippingMethod=shippingMethod,
-                    weight=round(order['total_weight'] / 453.592, 2),
-                    orderDate=order['created_at'],
-
-                    status=status,
-                    reference=reference,
-                    internalNote=internalNote,
-                    customerNote=order['note'],
+                        "shippingMethod": shippingMethod,
+                        "weight": round(order['total_weight'] / 453.592, 2),
+                        "orderDate": order['created_at'],
+                        "status": status,
+                        "reference": reference,
+                        "internalNote": internalNote,
+                        "customerNote": order['note'],
+                    }
                 )
 
                 # Line Items
@@ -148,8 +157,8 @@ class Processor:
                         product = Product.objects.get(
                             shopifyId=lineItem['product_id'])
                     except Product.DoesNotExist:
-                        debug.warn(
-                            PROCESS, f"Order #{order['order_number']} Product {lineItem['product_id']} not found.")
+                        print(
+                            f"Order #{order['order_number']} Product {lineItem['product_id']} not found.")
                         continue
 
                     if "Trade" in lineItem['variant_title']:
@@ -174,9 +183,17 @@ class Processor:
                             lineItem['grams'] / 453.592),
                     )
 
-                debug.log(
-                    PROCESS, f"PO {order['order_number']} has been synced.")
-
             with ThreadPoolExecutor(max_workers=25) as executor:
-                for order in orders:
-                    executor.submit(syncOrder, order)
+                future_to_order = {executor.submit(
+                    syncOrder, order): order for order in orders}
+
+                for future in as_completed(future_to_order):
+                    order = future_to_order[future]
+
+                    try:
+                        future.result()
+                        debug.log(
+                            PROCESS, f"PO {order['order_number']} has been synced.")
+                    except Exception as e:
+                        debug.warn(
+                            PROCESS, f"PO {order['order_number']} has been failed. {str(e)}")
