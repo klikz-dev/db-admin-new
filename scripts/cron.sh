1c28282445192a21254787730087f9a7d3c75014
Murrell <dev@klikz.us>
2024-04-25 19:07:39 -0400

    chore: cron

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -22,6 +22,9 @@
 0 5 * * * . /home/ubuntu/admin/venv/bin/activate && python3 /home/ubuntu/admin/manage.py roomvo > /dev/null 2>> /home/ubuntu/log/cron_error.log
 0 6 * * * . /home/ubuntu/admin/venv/bin/activate && python3 /home/ubuntu/admin/manage.py feed > /dev/null 2>> /home/ubuntu/log/cron_error.log
 
+# Send sample reminder at 12 pm daily
+0 12 * * * . /home/ubuntu/admin/venv/bin/activate && python3 /home/ubuntu/admin/manage.py klaviyo sample-reminder > /dev/null 2>> /home/ubuntu/log/cron_error.log
+
 # EDI - Brewster
 5 10,16 * * * . /home/ubuntu/admin/venv/bin/activate && python3 /home/ubuntu/admin/manage.py edi-brewster submit > /dev/null 2>> /home/ubuntu/log/cron_error.log
 5 9,15 * * * . /home/ubuntu/admin/venv/bin/activate && python3 /home/ubuntu/admin/manage.py edi-brewster ref > /dev/null 2>> /home/ubuntu/log/cron_error.log
@@ -29,6 +32,7 @@
 # EDI - Kravet
 10 10,16 * * * . /home/ubuntu/admin/venv/bin/activate && python3 /home/ubuntu/admin/manage.py edi-kravet submit > /dev/null 2>> /home/ubuntu/log/cron_error.log
 10 9,15 * * * . /home/ubuntu/admin/venv/bin/activate && python3 /home/ubuntu/admin/manage.py edi-kravet ref > /dev/null 2>> /home/ubuntu/log/cron_error.log
+10 11,17 * * * . /home/ubuntu/admin/venv/bin/activate && python3 /home/ubuntu/admin/manage.py edi-kravet tracking > /dev/null 2>> /home/ubuntu/log/cron_error.log
 
 # EDI - Phillips Collection
 15 10,16 * * * . /home/ubuntu/admin/venv/bin/activate && python3 /home/ubuntu/admin/manage.py edi-phillipscollection submit > /dev/null 2>> /home/ubuntu/log/cron_error.log
@@ -39,6 +43,7 @@
 # EDI - Schumacher
 25 10,16 * * * . /home/ubuntu/admin/venv/bin/activate && python3 /home/ubuntu/admin/manage.py edi-schumacher submit > /dev/null 2>> /home/ubuntu/log/cron_error.log
 25 9,15 * * * . /home/ubuntu/admin/venv/bin/activate && python3 /home/ubuntu/admin/manage.py edi-schumacher ref > /dev/null 2>> /home/ubuntu/log/cron_error.log
+25 11,17 * * * . /home/ubuntu/admin/venv/bin/activate && python3 /home/ubuntu/admin/manage.py edi-schumacher tracking > /dev/null 2>> /home/ubuntu/log/cron_error.log
 
 # EDI - York
 30 10,16 * * * . /home/ubuntu/admin/venv/bin/activate && python3 /home/ubuntu/admin/manage.py edi-york submit > /dev/null 2>> /home/ubuntu/log/cron_error.log
